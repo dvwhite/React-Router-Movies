98a24020786169f838d6340a034989069f45e5d2
David White <dwhite0627@gmail.com>
2020-01-12 18:22:35 -0500

    Implement save button functionality for the SavedList component

--- client/src/App.js
+++ client/src/App.js
@@ -16,8 +16,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path='/' component={MovieList} />
-      <Route path='/movies/:id' component={Movie} />
+      <Route 
+        exact path='/' 
+        component={MovieList}
+      />
+      <Route 
+        path='/movies/:id' 
+        render={props => <Movie {...props} addToSavedList={addToSavedList} />} 
+      />
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -20,11 +20,10 @@ const Movie = (props) => {
 
   },[props.match.params.id]);
   
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -49,7 +48,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
